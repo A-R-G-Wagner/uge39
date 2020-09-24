89557e8204748e445aeb2ba10f14669555782753
A-R-G-Wagner <60380845+A-R-G-Wagner@users.noreply.github.com>
2020-09-24 15:23:10 +0200

    Add files via upload

---
+++ rest-jpa-devops-startcode/src/main/java/rest/PersonResource.java
@@ -0,0 +1,70 @@
+package rest;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import dtos.PersonDTO;
+import facades.PersonFacade;
+import utils.EMF_Creator;
+import javax.persistence.EntityManagerFactory;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+//Todo Remove or change relevant parts before ACTUAL use
+@Path("person")
+public class PersonResource {
+
+    private static final EntityManagerFactory EMF = EMF_Creator.createEntityManagerFactory();
+
+    private static final PersonFacade FACADE = PersonFacade.getPersonFacade(EMF);
+    private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
+
+    @Path("{id}")
+    @GET
+    @Produces({MediaType.APPLICATION_JSON})
+    public String getPerson(@PathParam("id") int id) {
+
+        PersonDTO p = FACADE.getPerson(id);
+
+        return GSON.toJson(p);
+    }//getPerson
+
+    @POST
+    @Produces({MediaType.APPLICATION_JSON})
+    @Consumes({MediaType.APPLICATION_JSON})
+    public String addPerson(String person) {
+        PersonDTO p = GSON.fromJson(person, PersonDTO.class);
+
+        PersonDTO newP = FACADE.addPerson(p.getFirstName(), p.getLastName(), p.getPhone());
+        
+        return GSON.toJson(newP);
+    }//addPerson
+
+    @PUT
+    @Path("{id}")
+    @Produces({MediaType.APPLICATION_JSON})
+    @Consumes({MediaType.APPLICATION_JSON})
+    public String updatePerson(@PathParam("id") int id, String person) {
+        PersonDTO p = GSON.fromJson(person, PersonDTO.class);
+        p.setId(id);
+        PersonDTO newP = FACADE.editPerson(p);
+        
+        return GSON.toJson(newP);
+    }//updatePerson
+
+    @DELETE
+    @Path("{id}")
+    @Produces({MediaType.APPLICATION_JSON})
+    public String deletePerson(@PathParam("id") int id) {
+        PersonDTO deletedP = FACADE.deletePerson(id);
+
+        return GSON.toJson(deletedP);
+    }
+
+}//class
